b0e4c8195435593a3030eeec3faed1cabdc1ce52
dbarman <doyelbarman@gmail.com>
2015-07-26 12:56:04 -0400

    Commit 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix contains 4 functions: set, get, setmatrix, getmatrix.
+## get is a function that returns the matrix x stored in the main function.
+##  et is a function that changes the matrix stored in the main function.
+## setmatrix and getmatrix are functions very similar to set and get. 
+## They don't calculate the inverse, they simply store the value of the input 
+## in a variable m into the main function makeCacheMatrix (setmatrix) and return it (getmatrix).
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -20,7 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Input of cacheSolve is the object where makeCacheMatrix is stored. 
+## The first thing cacheSolve does is to verify the value inv, 
+## stored previously with getmatrix, exists and is not NULL. 
+## If it exists in memory, it simply returns a message and the value inv, 
+## that is supposed to be the inverse, but not necessarily.
 
 cacheSolve <- function(x, ...) {
   inv<-x$getmatrix()
